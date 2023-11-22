7c84565b9fe214a0083b7a403da625fb31aa18d1
samuelevalpertaa <60710482+samuelevalperta@users.noreply.github.com>
2023-11-22 01:38:24 +0100

    [olHel Wdorl] Update and rename decompiled to HelloWorld.c

--- olHel-Wdorl/HelloWorld.c
+++ olHel-Wdorl/HelloWorld.c
@@ -1,4 +1,7 @@
-int __cdecl main(int argc, const char **argv, const char **envp)
+#include <stdio.h>
+#include <stdint.h>
+
+int main()
 {
     uint64_t v1= 0x726F6C6564574820;
     uint64_t v2 = 0xBF342C370;
